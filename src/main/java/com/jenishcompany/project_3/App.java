b1cb33470a19b8cf4f52b586e13120535d867d7d
unknown <Jenish Josha>
2024-03-26 12:39:21 +0530

    added initial commit

---
+++ src/main/java/com/jenishcompany/project_3/App.java
@@ -0,0 +1,13 @@
+package com.jenishcompany.project_3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
